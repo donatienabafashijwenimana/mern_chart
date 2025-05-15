e9a5b7d836bee879b6d3dffdac3ce13effe565e0
donatienabafashijwenimana <donaabafashijwe@gmail.com>
2025-05-15 20:37:12 +0200

    commited by adn

--- server/LIB/socketserver.js
+++ server/LIB/socketserver.js
@@ -8,7 +8,8 @@ const server =  http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-       origin:["http://localhost:3000"],
+       origin:process.env.frontend_url|| '*',
+       methods:['GET','POST'],
        Credential:true
     }
 })
